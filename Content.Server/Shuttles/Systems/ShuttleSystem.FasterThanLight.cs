e84cb90f318f7a230192516500a456413b69da30
Vanessa <908648+ShepardToTheStars@users.noreply.github.com>
2026-01-28 15:58:52 -0600

    Vessel Grid Name Overlay, Echo Ambience, Dynamic Biome Ambient Music (#391)
    * Grid Name Display (#104)
    
    Signed-off-by: ark1368 <189933909+ark1368@users.noreply.github.com>
    
    * Metadata Fix (#255)
    
    * Area Based Sound Echo [Goob/Monolith Port] (#2452)
    
    * Initial
    
    * chore: automatically update REUSE headers
    
    * Test
    
    ---------
    
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>
    
    * Faction-Specific Combat Music, Biome System, Biome-Specific Music, Station Music, Text Descriptions of Vessels and Biomes. (#2981)
    
    * Added a bunch of placeholder vessel descriptions.
    
    * Locale fix for options menu
    
    ---------
    
    Signed-off-by: ark1368 <189933909+ark1368@users.noreply.github.com>
    Co-authored-by: Ark <189933909+ark1368@users.noreply.github.com>
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>
    Co-authored-by: Alin <33970022+alin-but-real@users.noreply.github.com>

--- Content.Server/Shuttles/Systems/ShuttleSystem.FasterThanLight.cs
+++ Content.Server/Shuttles/Systems/ShuttleSystem.FasterThanLight.cs
@@ -30,6 +30,7 @@ using Robust.Shared.Physics.Components;
 using Robust.Shared.Player;
 using Robust.Shared.Utility;
 using FTLMapComponent = Content.Shared.Shuttles.Components.FTLMapComponent;
+using Robust.Shared.Prototypes;
 
 namespace Content.Server.Shuttles.Systems;
 
@@ -90,6 +91,8 @@ public sealed partial class ShuttleSystem
     private EntityQuery<BodyComponent> _bodyQuery;
     private EntityQuery<FTLSmashImmuneComponent> _immuneQuery;
     private EntityQuery<StatusEffectsComponent> _statusQuery;
+
+    [Dependency] private readonly IEntityManager _entManager = default!; // Mono
 
     private void InitializeFTL()
     {
